8095488e54063eb56c3d66c87ffc394abed1bc75
samyr silva <ssamyrsilva.santana@gmail.com>
2025-02-10 17:08:12 +0000

    other page all done

--- support.js
+++ support.js
@@ -9,12 +9,29 @@ function getQueryParam(param) {
 
 // Get time from URL (in seconds)
 let timeLeft = parseInt(getQueryParam("time"));
+let theReal = getQueryParam("name");
+let url = window.location.href;
 
 const alarmSound = document.getElementById("alarmSound"); // Get the audio element
 const timerElement = document.getElementById("timer");
+const nameObject = document.getElementById("name");
 
+function changeName(){
+    if(theReal == "rice"){
+        nameObject.textContent = "Your rice is ready in...";
+    }
+    else if(theReal == "feijao"){
+        nameObject.textContent = "Your baked bean is ready in...";
+    }
+    else{
+        nameObject.textContent = "Your cuzcuz is ready in...";
+    }
+}
 // Countdown function
 function startTimer() {
+    if(url.includes("others")){
+        changeName();
+    }
     if (timeLeft > 0) {
         let minutes = Math.floor(timeLeft / 60);
         let seconds = timeLeft % 60;
